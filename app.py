7fd87497bdb501ebdae694308c028388d3d97c31
filip.m <filamalek@hotmail.com>
2018-05-20 13:52:59 +0200

    Added asymmetric enc

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import hashlib
 import bcrypt
 # symetric encryption for saving notes
 import nacl, nacl.secret, nacl.utils
-from nacl.public import PrivateKey, SealedBox
+from nacl.public import PrivateKey, SealedBox, PublicKey
 from binascii import hexlify
 
 # database vars
@@ -21,6 +21,7 @@ client = MongoClient('mongodb://localhost:27017/')
 db = None
 users = None
 notes = None
+secret_key = None
 
 # hashing
 salt = bcrypt.gensalt(rounds = 12)
@@ -37,10 +38,12 @@ def initDatabase():
     global db
     global users
     global notes
+    global secret_key
     # Get the database and users collection
     db = client.crypto_db
     users = db.users
     notes = db.notes
+    secret_key = db.secret_key
     if db:
         print('Connection do db success...')
         return True
@@ -52,35 +55,55 @@ def initDatabase():
 def insertUser(user):
     global salt
     global users
-    global box
+    # global box
+    global secret_key
     # hashing a password just using bcrypt
     hashed = bcrypt.hashpw(user['password'].encode('utf-8'), salt)
 
     # saving hashed password back into user object
     user['password'] = hashed
 
-    # generate a random 32 byte key
-    userSecretKey = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+    # GENERATE PRIVATE KEY
+    secretKey = PrivateKey.generate()
 
-    user['key'] = userSecretKey
-    users.insert_one(user)
+    # GENERATE PUBLIC KEY BASED ON THE PRIVATE KEY
+    publicKey = secretKey.public_key
+
+    # user['key'] = userSecretKey
+    # save user
+    isUserSaved = users.insert_one(user)
+    if isUserSaved:
+        # create key pair json in another table
+        jsonToSave = {
+            'username': user['username'],
+            'secretKey': secretKey.encode(),
+            'publicKey': publicKey.encode()
+        }
+        # insert into collection
+        secret_key.insert_one(jsonToSave)
 
 # adds a note to database
 def addNote(noteJson):
     global notes
-    global box
+    # global box
+    global userKey
+
     text = noteJson['note']
-    key = noteJson['key']
+    username = noteJson['username']
+    publicKey = userKey['publicKey']
 
-    # store key in the safe
-    box = nacl.secret.SecretBox(key)
-    # generate nonce for extra randomness
-    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-    # finally encrypt the message
-    encrypted = box.encrypt(text, nonce)
+    # create a box for encryption
+    sealBox = SealedBox(publicKey)
 
+    # encrypt the message
+    encrypted = sealBox.encrypt(text)
 
-    if notes.insert_one({'note': encrypted}):
+    noteToInsert = {
+        'username': username,
+        'note': encrypted
+    }
+
+    if notes.insert_one(noteToInsert):
         return True
     else:
         return False
@@ -97,15 +120,30 @@ def login(userToLogin):
     global users
     global salt
     global userKey
+    global secret_key
     # finding object with the same username
     user = users.find_one({'username': userToLogin['username']})
     # if user exists
     if user:
         if bcrypt.hashpw(userToLogin['password'].encode('utf-8'), user['password']) == user['password']:
-            userKey = user['key']
-            print('This is the user key')
-            print(userKey)
-            return True
+            # userKey = user['key']
+            # print('This is the user key')
+            # print(userKey)
+
+            # find his keys
+            keys = secret_key.find_one({'username': user['username']})
+            if keys:
+                # turning byte key values to proper types for SealedBox
+                pubKey = PublicKey(keys['publicKey'])
+                secKey = PrivateKey(keys['secretKey'])
+
+                userKey = {
+                    'publicKey': pubKey,
+                    'secretKey': secKey
+                }
+                # print(type(pubKey))
+                # print(type(secKey))
+                return True
         else:
             # print('no match')
             return False
@@ -113,15 +151,17 @@ def login(userToLogin):
         print('User cannot be found')
 
 # read notes
-def readNotes():
+def readNotes(username):
     # key to decrypt the messages
     global userKey
     global notes
 
-    for note in notes.find():
-        # store key in the safe
-        box = nacl.secret.SecretBox(userKey)
-        decryptedNote = box.decrypt(note['note'])
+    # create a unseal box
+    unsealBox = SealedBox(userKey['secretKey'])
+
+    print(username)
+    for note in notes.find({'username': username}):
+        decryptedNote = unsealBox.decrypt(note['note'])
 
         print(decryptedNote)
 
@@ -172,14 +212,14 @@ while True:
         note = input('Add a note: ').encode('utf-8')
         noteJson = {
             'note': note,
-            'key': userKey
+            'username': userJson['username']
         }
         if addNote(noteJson):
             print('Note added!')
         else:
             print('Note not added')
     if loggedIn and choice == '2':
-        readNotes()
+        readNotes(userJson['username'])
     if loggedIn and choice == '3':
         break
 
